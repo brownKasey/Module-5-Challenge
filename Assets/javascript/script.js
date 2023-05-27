2ca44f36676abe418caf2ae961443b16eaf094cc
unknown <kaseybrown28@icloud.com>
2023-05-26 21:52:11 -0400

    added several variables in java, added the time to the page, added code to change color of hour

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,7 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var today = dayjs();
+var containerEl = $('.px-5');
+var hourEl = containerEl.children();
+var currentHour = dayjs().get('hour');
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -12,22 +14,25 @@ $(function () {
 
   // <will need to give divs a class of past, present, or future based on the time>//
   // <the javascript will need to do this by adding/removing these classes on each div by comparing the hour in the id to the current hour.//
-
-
-
+  
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  function time(){
+  
+    // adds class to hour
+    //hourEl.addClass('past');
+    //hourEl.addClass('present');
+    //hourEl.addClass('future');
 
-    var timeInterval = setInterval(function() {
-      $('#currentDay').text(today.format('dddd, MMMM DD hh:mm:ss'));
-    }, 1000);
-  }
-time();
+
+
+
+
+
+
 
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
@@ -35,6 +40,13 @@ time();
   // attribute of each time-block be used to do this?
   //
   //displays the current date in the header of the page.
+  function showTime(){
 
+    var timeInterval = setInterval(function() {
+      $('#currentDay').text(dayjs().format('dddd, MMMM DD hh:mm:ss'));
+    }, 1000);
+  }
+showTime();
+  
 
 });
